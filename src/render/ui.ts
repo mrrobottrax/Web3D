2d22efc94c2aa8b135008883a26b654144febcca
mrrobottrax <59744880+mrrobottrax@users.noreply.github.com>
2023-10-17 17:05:20 -0400

    add gun image

--- src/render/ui.ts
+++ src/render/ui.ts
@@ -1,19 +1,22 @@
 import { mat4 } from "../math/matrix.js";
 import { vec3 } from "../math/vector.js";
-import { SharedAttribs, gl, uiShader } from "./gl.js";
+import { SharedAttribs, gl, loadTexture, uiShader } from "./gl.js";
 import { uiMatrix } from "./render.js";
 
 let vao: WebGLVertexArrayObject | null;
 let squareVertsBuffer: WebGLBuffer | null;
 
 const squareVerts: number[] = [
-	-0.5, -0.5, 1, -1, -1,
-	0.5, -0.5, 1, 1, -1,
-	0.5, 0.5, 1, 1, 1,
-	-0.5, 0.5, 1, -1, 1,
+	-0.5, -0.5, 1, 0, 1,
+	0.5, -0.5, 1, 1, 1,
+	0.5, 0.5, 1, 1, 0,
+	-0.5, 0.5, 1, 0, 0,
 ];
 
-let viewModelLocation = new vec3(0, 0, 0);
+let viewModelLocation = new vec3(0.8, -0.65, 0);
+let viewModelScale = new vec3(0.75, 0.75, 0.75);
+
+let gunTex: WebGLTexture;
 
 export function initUi() {
 	vao = gl.createVertexArray();
@@ -31,23 +34,37 @@ export function initUi() {
 	gl.enableVertexAttribArray(SharedAttribs.texCoordAttrib);
 
 	gl.bindVertexArray(null);
+
+	loadTexture("data/textures/fire0.png").then(
+		(value) => {
+			if (value) {
+				gunTex = value;
+			}
+		}
+	);
 }
 
 export function drawUi() {
 	gl.useProgram(uiShader.program);
 	gl.bindVertexArray(vao);
 
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, gunTex);
+	
 	gl.uniformMatrix4fv(uiShader.projectionMatrixUnif, false, uiMatrix.getData());
 	gl.uniform4f(uiShader.colorUnif, 1, 1, 1, 1);
 	gl.uniform1i(uiShader.samplerUnif, 0);
-
+	
 	let mat;
 	
 	// draw viewmodel
 	mat = mat4.identity();
 	mat.translate(viewModelLocation);
+	mat.scale(viewModelScale);
 	gl.uniformMatrix4fv(uiShader.modelViewMatrixUnif, false, mat.getData());
 	gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+	
+	gl.bindTexture(gl.TEXTURE_2D, null);
 
 	gl.bindVertexArray(null);
 	gl.useProgram(null);
