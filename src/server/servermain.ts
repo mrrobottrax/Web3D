8085a383634d8a69907b0fbe38d7d6ce9e92a398
Andrew <mrrobottrax@gmail.com>
2023-10-24 21:15:40 -0400

    make server open with debug

--- src/server/servermain.ts
+++ src/server/servermain.ts
@@ -3,11 +3,13 @@ import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 27900 });
 
 wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+	ws.on('error', console.error);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
+	ws.on('message', function message(data) {
+		console.log('received: %s', data);
+	});
 
-  ws.send('something');
+	ws.send('something');
 });
+
+console.log("SERVER OPENED");
