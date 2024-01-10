3ce54864c4e70dac6f07fba5a221669cbf25b4b7
Andrew <mrrobottrax@gmail.com>
2024-01-10 13:32:30 -0500

    Allow entities to have parents

--- src/common/entitysystem/update.ts
+++ src/common/entitysystem/update.ts
@@ -6,11 +6,26 @@ export function updateEntities() {
 		entity.update();
 
 		// update positions
-		const trans = entity.transform;
-		const mat = trans.worldMatrix;
-		mat.setIdentity();
-		mat.translate(trans.translation);
-		mat.rotate(trans.rotation);
-		mat.scale(trans.scale);
+		// we update entities with parents later
+		if (!entity.parent) {
+			const trans = entity.transform;
+			const mat = trans.worldMatrix;
+			mat.setIdentity();
+			mat.translate(trans.translation);
+			mat.rotate(trans.rotation);
+			mat.scale(trans.scale);
+		}
+	}
+
+	// update entities with parent
+	for (const entity of entities) {
+		if (entity.parent) {
+			const trans = entity.transform;
+			const mat = trans.worldMatrix;
+			mat.set(entity.parent.transform.worldMatrix);
+			mat.translate(trans.translation);
+			mat.rotate(trans.rotation);
+			mat.scale(trans.scale);
+		}
 	}
 }
